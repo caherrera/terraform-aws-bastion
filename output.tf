1b820d6ff48d2a0a1f12bf6a64efc9813afc87ca
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2022-11-25 16:45:14 -0300

    adding outputs

--- output.tf
+++ output.tf
@@ -2,3 +2,15 @@ output "private_ip" {
   description = "The private IP address assigned to the instance."
   value       = module.bastion.private_ip
 }
+
+output "public_ip" {
+  value = module.bastion.public_ip
+}
+
+output "private_dns" {
+  value = module.bastion.private_dns
+}
+
+output "public_dns" {
+  value = module.bastion.public_dns
+}
